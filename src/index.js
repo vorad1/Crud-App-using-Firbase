17c6159bc48b31f7438a3fe19182ae61e5aee68e
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-11-11 12:10:38 +1300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/edit/:id' component={Edit} />
+        <Route path='/create' component={Create} />
+        <Route path='/show/:id' component={Show} />
+      </div>
+  </Router>,
   document.getElementById('root')
 );
 
